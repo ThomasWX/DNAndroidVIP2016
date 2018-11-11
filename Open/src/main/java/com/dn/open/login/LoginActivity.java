5eda9b4ecc98936671a013500a017091987b5abb
ThomasWX <ThomasWb@163.com>
2018-11-12 01:48:58 +0800

    渲染

---
+++ Open/src/main/java/com/dn/open/login/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.dn.open.login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 双十一来袭，手机淘宝如何设计登录架构—今天带你走进的登录拦截
+ * 淘宝登录架构之登录拦截
+ */
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
---
+++ Open/src/main/java/com/dn/open/optimization/rendering/RenderingActivity.java
@@ -0,0 +1,15 @@
+package com.dn.open.optimization.rendering;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 渲染优化公开课程
+ */
+public class RenderingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
